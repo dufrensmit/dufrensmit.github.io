f9aaad0e75b56553b2ee1e6fd4088b151dfc4d63
dufrensmit <lucaslowen10@gmail.com>
2026-09-22 18:09:57 -0300

    exercicio 2

--- js/exercicio2.js
+++ js/exercicio2.js
@@ -3,5 +3,17 @@ function tratadorDeCliqueExercicio2() {
     // exibir um alerta com a hora 
     // atual no seguinte formato:
     // Horário: 8 PM : 40m : 28s
+
+    const agora = new Date();
+
+    let hora = agora.getHours();
+    const periodo = hora < 12 ? "AM" : "PM";
+    hora = hora % 12 || 12;
+
+    const minuto = agora.getMinutes();
+    const segundo = agora.getSeconds();
+
+    alert(`Horário: ${hora} ${periodo} : ${minuto}m : ${segundo}s`);
+
     console.log('adicionar código na função tratadorDeCliqueExercicio2() em ./js/exercicio2.js')
 }
