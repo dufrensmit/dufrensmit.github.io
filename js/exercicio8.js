ed808e7b65c6616f9dbf5926046390c3f7145f08
dufrensmit <lucaslowen10@gmail.com>
2026-09-22 19:55:07 -0300

    ex 8

--- js/exercicio8.js
+++ js/exercicio8.js
@@ -1,6 +1,20 @@
 function obterRegiaoFiscalAtravesDoCPFInformado(cpfInformado) {
     //edite esta função!
+
     let regiaoFiscal = undefined
+
+    let cpf_string = cpfInformado.toString()
+    
+    let digito_regiao = cpf_string[8]
+
+
+    if (digito_regiao == "0"){
+        regiaoFiscal = 9
+    }
+    else {
+        regiaoFiscal = parseInt(digito_regiao) - 1
+    }
+        
     console.log(cpfInformado)
     
     //----------------------------
